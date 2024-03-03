037cb104375a5288a8a54f24072f9cb7434d456f
elopezfilippini <elopezfilippini@gmail.com>
2024-03-02 21:06:27 -0300

    cmbio en cors

--- src/app.js
+++ src/app.js
@@ -24,7 +24,7 @@ server.options('/*', (req, res) => {
 });
 
   server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+    res.header('Access-Control-Allow-Origin', 'https://dogsfront-8.onrender.com'); // update to match the domain you will make the request from
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
